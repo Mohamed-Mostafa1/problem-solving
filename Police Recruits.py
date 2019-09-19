dc55512cc22b01b9437c91c3016ccb98add51a58
Mohamed <mohamed.mostafa1995@hotmail.com>
2019-09-19 18:17:58 +0200

    back again :D

--- Police Recruits.py
+++ Police Recruits.py
@@ -1,3 +1,13 @@
+avlCops, result  = [0, 0]
 n = input()
 crimes = list(map(int, input().split()))
+for crime in crimes:
+    if crime > 0 :
+        avlCops += crime
+    else:
+        if avlCops:
+            avlCops += crime
+        else:
+            result += 1
+print(result)
 
